a50abf36c271d752d67e607680208d6676315b13
shravanrn@gmail.com <shravanrn@gmail.com>
2020-02-07 19:20:02 -0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 g++ -std=c++14 `pkg-config --cflags apr-1` -fPIC -DPIC -I$LIBMARKDOWN -I/usr/include/httpd -fpermissive -Wl,--export-dynamic -lstdc++ -lsupc++ -ldl -c mod_markdown.cpp 
-/usr/share/apr-1/build/libtool --silent --mode=link g++ -std=c++14 -Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -o mod_markdown.la  -lapr-1 -rpath /usr/lib/httpd/modules -lstdc++ -lsupc++ -ldl -module -avoid-version  mod_markdown.o
+libtool --silent --mode=link g++ -std=c++14 -Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -o mod_markdown.la  -lapr-1 -rpath /usr/lib/httpd/modules -lstdc++ -lsupc++ -ldl -module -avoid-version  mod_markdown.o
